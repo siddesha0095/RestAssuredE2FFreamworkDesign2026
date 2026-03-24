0181c9ff1b14063f038ff7c2414425de5374cc1f
siddesha <siddesha0095@gmail.com>
2026-03-24 11:11:42 +0530

    login api updated

--- src/test/java/com/api/tests/LoginAPITest2.java
+++ src/test/java/com/api/tests/LoginAPITest2.java
@@ -2,20 +2,21 @@ package com.api.tests;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import static io.restassured.RestAssured.*;
+
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
 
-public class LoginAPITest {
+public class LoginAPITest2 {
 
     @Test(description = "Verify if Login API is working...")
     public void loginTest() {
-        RestAssured.baseURI = "http://64.227.160.186:8080";
-        RequestSpecification x = RestAssured.given();
-        RequestSpecification y = x.header("Content-Type", "application/json");
-        RequestSpecification z = y.body("{\"username\": \"siddesha0095\",\"password\": \"Navaneeth22#\"}");
-        Response response = z.post("/api/auth/login");
+        //baseURI = "http://64.227.160.186:8080"; // used static import so no need of RestAssured.baseURI
+        
+       Response response = given().baseUri("http://64.227.160.186:8080")
+    		   .header("Content-Type", "application/json").body("{\"username\": \"siddesha0095\",\"password\": \"Navaneeth22#\"}").post("/api/auth/login");
         System.out.println(response.asPrettyString());
         
         Assert.assertEquals(response.getStatusCode(), 200);
