8fc3a064561286df4d88389f2d890c472eb71520
siddesha <siddesha0095@gmail.com>
2026-03-24 10:37:07 +0530

    first commit framework

---
+++ src/test/java/com/api/tests/LoginAPITest.java
@@ -0,0 +1,24 @@
+package com.api.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+
+public class LoginAPITest {
+
+    @Test(description = "Verify if Login API is working...")
+    public void loginTest() {
+        RestAssured.baseURI = "http://64.227.160.186:8080";
+        RequestSpecification x = RestAssured.given();
+        RequestSpecification y = x.header("Content-Type", "application/json");
+        RequestSpecification z = y.body("{\"username\": \"uday1234\",\"password\": \"uday1234\"}");
+        Response response = z.post("/api/auth/login");
+        System.out.println(response.asPrettyString());
+        
+        Assert.assertEquals(response.getStatusCode(), 200);
+    }
+}
+
